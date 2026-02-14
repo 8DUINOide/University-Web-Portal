f1720e39d1f3e626d017de57c428e3331c15456d
Al Francis B. Paz <afpaz@gbox.adnu.edu.ph>
2026-02-14 12:26:01 +0800

    move public folder to root for better gh pages support

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the 'public' folder
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the root folder
+app.use(express.static(__dirname));
 
 // Route to serve the homepage
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Start the server
